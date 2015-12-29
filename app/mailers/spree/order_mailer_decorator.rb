b89049b5d611169933502cbfbd9e625c93073d98
Ado <adrianbeyerle@gmail.com>
2015-12-29 15:44:50 +1100

    add email cc

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -3,7 +3,8 @@ Spree::OrderMailer.class_eval do
     @gift_card = Spree::GiftCard.find(card_id)
     @order = Spree::Order.find(order_id)
     subject = "#{Spree::Config[:site_name]} Gift Card"
+    admin_email = "#{Spree::Config[:admin_email]} Gift Card"
     @gift_card.update_attribute(:sent_at, Time.now)
-    mail(:to => @gift_card.email, :from => from_address, :subject => subject)
+    mail(:to => @gift_card.email, :from => from_address, :cc => admin_email,:subject => subject)
   end
 end
